9bc1d6dda1f36cbb2d69fa26a8655fade230c949
Sumit Kumar Shakya <sumitshakya20101999@gmail.com>
2023-01-28 16:35:55 +0530

    cors update

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const app = require('./app')
-    // const { connectDatabase } = require('./config/database')
+const cors = require('cors');
+// const { connectDatabase } = require('./config/database')
 
 
 // connectDatabase()
 
+app.use(cors({
+    origin: ['http://localhost:3000',
+        '*'
+    ]
+}));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on ${process.env.PORT}`)
